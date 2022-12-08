65758d31b3bfbe94033428936371d481873d2cb1
lexasub <you@example.com>
2022-12-08 05:17:45 +0400

    08.12

--- src/main/java/org/lexasub/IR1/IR1Block/IR1BaseBlock.java
+++ src/main/java/org/lexasub/IR1/IR1Block/IR1BaseBlock.java
@@ -15,7 +15,10 @@ public class IR1BaseBlock {
     public LinkedList<IR1BaseBlock> nodesOut = new LinkedList<>();
     public LinkedList<IR1BaseBlock> nodesInParents = new LinkedList<>();
     public LinkedList<IR1BaseBlock> nodesOutChilds = new LinkedList<>();
-    public IR1BaseBlock(){}
+
+    public IR1BaseBlock() {
+    }
+
     public IR1BaseBlock(FrontendBaseBlock v) {
         name = v.name;
         code = v.code;
@@ -29,69 +32,85 @@ public class IR1BaseBlock {
 
 
     public static IR1BaseBlock makeFromFrontendBaseBlock(FrontendBaseBlock frontendBlock) {
-        return makeFromFrontendBaseBlock(frontendBlock, new HashMap<>(), new IR1BaseBlock(frontendBlock));
+        return makeFromFrontendBaseBlock(frontendBlock, new HashMap<>());
     }
-    private static IR1BaseBlock makeFromFrontendBaseBlock(FrontendBaseBlock frontendBlock, HashMap<String, IR1BaseBlock> decls, IR1BaseBlock ir1BB) {
+
+    private static IR1BaseBlock makeFromFrontendBaseBlock(FrontendBaseBlock frontendBlock, HashMap<String, IR1BaseBlock> _decls) {
         //clone for cancel local modification(mb scopes)(ex local variables)//TODO check
-        getIr1BaseBlock(frontendBlock, decls, ir1BB).forEach(ir1BaseBlock -> connectToChilds(ir1BaseBlock, ir1BB));
+        IR1BaseBlock ir1BB = new IR1BaseBlock(frontendBlock);
+        HashMap<String, IR1BaseBlock> decls = (HashMap<String, IR1BaseBlock>) _decls.clone();
+        LinkedList<FrontendBaseBlock> fbChilds = frontendBlock.childs;
+        int cnt = 0;
+        /* if (Objects.equals(frontendBlock.blockId, "zlJ5kusc6q")) {
+            System.out.println(frontendBlock.name);
+        }*/
+        if (ir1BB.type == FrontendBaseBlock.TYPE.FUNC)
+            cnt = addFuncArgs(ir1BB, decls, fbChilds.iterator());
+        if (ir1BB.type == FrontendBaseBlock.TYPE.CODE)
+            codeBlock(ir1BB, decls, fbChilds);
+      //  else
+            getIr1BaseBlock(decls, fbChilds.stream().skip(cnt)).forEach(ir1BaseBlock -> connectToChilds(ir1BaseBlock, ir1BB));
         return ir1BB;
     }
 
+    private static IR1BaseBlock findOrCreateBlock(HashMap<String, IR1BaseBlock> decls, FrontendBaseBlock i) {
+        IR1BaseBlock ir1BaseBlock = decls.get(i.name);
+        if (ir1BaseBlock == null)
+            ir1BaseBlock = makeFromFrontendBaseBlock(i, decls);
+        return ir1BaseBlock;
+        //may be dependence with orig var
+    }
 
-    private static LinkedList<IR1BaseBlock> getIr1BaseBlock(FrontendBaseBlock frontendBlock, HashMap<String, IR1BaseBlock> _decls, IR1BaseBlock ir1BB) {
-        HashMap<String, IR1BaseBlock> decls = (HashMap<String, IR1BaseBlock>) _decls.clone();
-        LinkedList<FrontendBaseBlock> fbChilds = frontendBlock.childs;
-        int cnt = addFuncArgs(ir1BB, decls, fbChilds.iterator());
-        autoDeclare(ir1BB, decls, fbChilds);
-     //   if (ir1BB.type == FrontendBaseBlock.TYPE.CODE) cnt+=2;
-            Stream<IR1BaseBlock> ir1BaseBlockStream = fbChilds.stream().skip(cnt) //skip funcArgs
+    private static LinkedList<IR1BaseBlock> getIr1BaseBlock(HashMap<String, IR1BaseBlock> decls, Stream<FrontendBaseBlock> blocks) {
+        return new LinkedList<>(blocks //skip blocks
                 .map(i -> {
-                    IR1BaseBlock ir1BaseBlock;
-                    ir1BaseBlock = makeFromFrontendBaseBlock(i, decls, new IR1BaseBlock(i));
+                    IR1BaseBlock ir1BaseBlock = findOrCreateBlock(decls, i);
                     if (Objects.equals(ir1BaseBlock.type, FrontendBaseBlock.TYPE.BLOCK))
                         decls.put("res_" + ir1BaseBlock.blockId, ir1BaseBlock);
                     return ir1BaseBlock;
-                });
+                }).toList());
         //may be last expr - it's return if blockId != ""
-        return new LinkedList<>(ir1BaseBlockStream.toList());
     }
 
-    private static void autoDeclare(IR1BaseBlock ir1BB, HashMap<String, IR1BaseBlock> decls, LinkedList<FrontendBaseBlock> args) {
-          if (ir1BB.type == FrontendBaseBlock.TYPE.CODE) {
-            //TODO
-            //extract from code ids and maybe add to nodesIn, nodesOut
-            connectTo(ir1BB, new IR1BaseBlock(args.get(0)));
+
+    private static void codeBlock(IR1BaseBlock ir1BB, HashMap<String, IR1BaseBlock> decls, LinkedList<FrontendBaseBlock> args) {
+        //TODO
+        //extract from code ids and maybe add to nodesIn, nodesOut
+        FrontendBaseBlock v = args.get(0);
+        connectTo(ir1BB, new IR1BaseBlock(v));
+        int n;
+        if (Objects.equals(v.name, "call")) {
             connectTo(ir1BB, new IR1BaseBlock(args.get(1)));
-            //skip (ex: call, name)
-            args.stream().skip(2).forEach(i->{
-                IR1BaseBlock ir1BaseBlock = decls.get(i.name);
+            n = 2;
+        } else n = 1;
+        //skip (ex: call, name)
 
-                if(ir1BaseBlock==null)
-                    return;
-                System.out.println(i.name + " " + i.blockId + " " + ir1BaseBlock.blockId);
-                //ok on read variable, on write to variable-it's wrong
-                //if == "res_...." -> вроде всегда получаем read.
-                // write на данном этапе в дереве не будет.(т.к.) auto-return(ex return last expr) not applyed
-                // ir1BB.nodesOutChilds.remove(ir1BaseBlock);
-                //connectToChilds(ir1BB, ir1BaseBlock);//mb_swap
-                //connectTo(ir1BB, ir1BaseBlock);//mb_swap
-            });
-        }
+        args.stream().skip(n).forEach(i -> {
+            IR1BaseBlock ir1BaseBlock = findOrCreateBlock(decls, i);
+            if (Objects.equals(ir1BaseBlock.type, FrontendBaseBlock.TYPE.BLOCK))
+                decls.put("res_" + ir1BaseBlock.blockId, ir1BaseBlock);
+            //System.out.println(i.name + " " + i.blockId + " " + ir1BaseBlock.blockId);
+            //ok on read variable, on write to variable-it's wrong
+            //if == "res_...." -> вроде всегда получаем read.
+            // write на данном этапе в дереве не будет.(т.к.) auto-return(ex return last expr) not applyed
+            // ir1BB.nodesOutChilds.remove(ir1BaseBlock);
+            //connectToChilds(ir1BB, ir1BaseBlock);//mb_swap
+            //connectTo(ir1BB, ir1BaseBlock);//mb_swap
+        });
     }
+
 
     private static int addFuncArgs(IR1BaseBlock ir1BB, HashMap<String, IR1BaseBlock> decls, Iterator<FrontendBaseBlock> childsIt) {
         int cnt = 0;
         //System.out.println(ir1BB.name + " " + ir1BB.blockId + " " + ir1BB.blockId);
-        if(ir1BB.type == FrontendBaseBlock.TYPE.FUNC) {
-            while (childsIt.hasNext()){
-                FrontendBaseBlock v = childsIt.next();
-                if(v.type != FrontendBaseBlock.TYPE.ID) break;
-                ++cnt;
-                IR1BaseBlock vv = new IR1BaseBlock(v);
-                connectToChilds(vv, ir1BB);
-                decls.put(v.name, vv);
-                //childs должен при type=ID быть пустым
-            }
+        while (childsIt.hasNext()) {
+            FrontendBaseBlock v = childsIt.next();
+            if (v.type != FrontendBaseBlock.TYPE.ID) break;
+            ++cnt;
+            IR1BaseBlock vv = new IR1BaseBlock(v);
+            connectToChilds(vv, ir1BB);
+            decls.put(v.name, vv);
+            //childs должен при type=ID быть пустым
         }
         return cnt;
     }
@@ -101,6 +120,7 @@ public class IR1BaseBlock {
         from.nodesOutChilds.add(to);
         to.nodesInParents.add(from);
     }
+
     public static void connectTo(IR1BaseBlock to, IR1BaseBlock from) {
         from.nodesOut.add(to);
         to.nodesIn.add(from);
